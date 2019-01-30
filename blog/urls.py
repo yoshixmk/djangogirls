03b71133d32a32cab85bcdbf0407fd7ebceae8af
YoshihiroUeki <pirori67@gmail.com>
2019-01-30 23:44:32 +0900

    fix import

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    # url(r'^$', views.post_list, name='post_list'),
     path('^$', views.post_list, name='post_list'),
 ]
