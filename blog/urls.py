67641715ec39b77fd3cfd1f7f15122c10c28b58e
YoshihiroUeki <pirori67@gmail.com>
2019-01-30 23:55:47 +0900

    create firstview

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    path('^$', views.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='post_list'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.urls import blog
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', blog.urls),
+    url(r'', include('blog.urls')),
 ]
