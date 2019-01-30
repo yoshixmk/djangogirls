8f5208a41d9f9cd9e655803e9c88ec3b22b96655
YoshihiroUeki <pirori67@gmail.com>
2019-01-30 23:40:13 +0900

    add url file

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import include, url
+from . import views
+
+urlpatterns = [
+    # url(r'^$', views.post_list, name='post_list'),
+    path('^$', views.post_list, name='post_list'),
+]
