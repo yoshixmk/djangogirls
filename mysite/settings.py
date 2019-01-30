8f90402eababd8419cc95c5046b0acd32e588d4f
YoshihiroUeki <pirori67@gmail.com>
2019-01-31 00:51:26 +0900

    heroku fix4

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,7 +42,9 @@ TEMPLATES = [
 
 # WSGI_APPLICATION = 'mysite.wsgi.application'
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
