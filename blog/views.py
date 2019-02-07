c32ef738ecd8d1de1c79cf52137abdba5e17588d
YoshihiroUeki <pirori67@gmail.com>
2019-02-08 01:08:28 +0900

    edit. and, octicon

--- blog/views.py
+++ blog/views.py
@@ -26,3 +26,18 @@ def post_new(request):
         post.save()
         return redirect(post_detail, pk=post.pk)
 
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect(post_detail, pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
